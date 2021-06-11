39b607f09aadde7de1d770f3517a29ae501ee82b
epi <epi.salamanca@gmail.com>
2021-06-11 22:24:11 +0100

    Update RemotingRemoteTransactionPeer.java

--- javaee/src/main/java/org/wildfly/transaction/client/provider/remoting/RemotingRemoteTransactionPeer.java
+++ javaee/src/main/java/org/wildfly/transaction/client/provider/remoting/RemotingRemoteTransactionPeer.java
@@ -24,8 +24,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.security.GeneralSecurityException;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CancellationException;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.net.ssl.SSLContext;
 import javax.transaction.SystemException;
@@ -93,7 +93,7 @@ class RemotingRemoteTransactionPeer implements RemoteTransactionPeer {
     ConnectionPeerIdentity getPeerIdentityXA() throws XAException {
         try {
             return getPeerIdentity();
-        } catch (IOException | RuntimeException e) {
+        } catch (IOException | CancellationException e) {
             throw Log.log.failedToAcquireConnectionXA(e, XAException.XAER_RMFAIL);
         }
     }
